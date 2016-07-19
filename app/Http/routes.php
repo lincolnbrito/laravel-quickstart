1611d839a6bc19d666a100b101072294af45d65b
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-19 15:13:00 -0300

    creating tasks routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
+/**
+ * Show Task Dashboard
+ */
 Route::get('/', function () {
-    return view('welcome');
+    //
+});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function(Request $reques){
+    //
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{task}', function(){
+    //
 });
